7af867b08934fc7b5120db18a0bb2229b54c6566
Nik <65584296+nikwithak@users.noreply.github.com>
2023-12-05 15:28:55 -0800

    Day 4 Part 2

--- src/bin/day_4.rs
+++ src/bin/day_4.rs
@@ -2,6 +2,8 @@ use std::{
     collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
+    ops::{Add, AddAssign},
+    sync::Mutex,
 };
 
 use regex::Regex;
@@ -10,14 +12,38 @@ use regex::Regex;
 mod tests {
     use super::*;
     #[test]
-    fn test_sample_input() {
+    fn test_part_1_sample_input() {
         let input = parse_input("inputs/day_4_sample.txt");
-        let result = count_points(input);
+        let result = count_points(&input);
         assert_eq!(13, result);
+    }
+
+    #[test]
+    fn test_part_2_sample_input() {
+        let input = parse_input("inputs/day_4_sample.txt");
+        let result = count_total_scratchcards(&input);
+        assert_eq!(30, result);
     }
 }
 
-fn count_points(cards: Vec<Card>) -> u64 {
+fn count_total_scratchcards(mut cards: &Vec<Card>) -> u64 {
+    let cards: Vec<(&Card, Mutex<u64>)> = cards.iter().map(|card| (card, Mutex::new(1))).collect(); // Count of each card
+    for (card, count) in &cards {
+        let matches = card.count_matches();
+        for i in 0..matches {
+            let (future_card, future_count) = &cards[(i + card.id) as usize];
+            assert_eq!(future_card.id, i + card.id + 1);
+            let mut future_count = future_count.lock().unwrap();
+            let count = count.lock().unwrap();
+            *future_count += *count;
+        }
+    }
+    let total_cards = cards.iter().fold(0, |acc, (_, count)| acc + *count.lock().unwrap());
+
+    total_cards
+}
+
+fn count_points(cards: &Vec<Card>) -> u64 {
     cards
         .iter()
         .map(|card| {
@@ -29,8 +55,10 @@ fn count_points(cards: Vec<Card>) -> u64 {
 
 fn main() {
     let input = parse_input("inputs/day_4.txt");
-    let result = count_points(input);
-    println!("Result: {}", result);
+    let points = count_points(&input);
+    let total_cards = count_total_scratchcards(&input);
+    println!("Points: {}", points);
+    println!("Total Cards: {}", total_cards);
 }
 
 #[derive(Debug)]
